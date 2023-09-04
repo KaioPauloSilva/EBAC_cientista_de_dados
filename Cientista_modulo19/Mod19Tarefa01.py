03e9c03de4a5a420e798db35167cc91396ca8480
Kaio Paulo <kaio.paulo50@gmail.com>
2023-09-04 17:05:41 -0300

    Modulo 19

---
+++ Cientista_modulo19/Mod19Tarefa01.py
@@ -0,0 +1,205 @@
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+import timeit
+
+
+@st.cache_data(show_spinner= True)
+def load_data(file_data):
+    try:
+        return pd.read_csv(file_data, sep=';')
+    except:
+        return pd.read_excel(file_data)
+
+
+@st.cache_data()
+def multiselect_filter(relatorio, col, selecionados):
+    if 'All' in selecionados:
+        return relatorio
+    else:
+        return relatorio[relatorio[col].isin(selecionados)].reset_index(drop=True)
+
+
+@st.cache_data
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+@st.cache_data()
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+
+
+def main():
+    st.set_page_config(page_title='Modulo19 Tarefa01',
+                       page_icon='📖',
+                       layout='wide')
+
+    #Cabeçalho
+    image = Image.open("./img/Bank-Branding.jpg")
+    st.image(image)
+    st.header('Telemarketing Analysis 🔎')
+    st.markdown("---")
+
+    #Upload de arquivo CSV OU EXCEL
+    data_file_1 = st.file_uploader("Bank marketing data",
+                                    type=['csv', 'xlsx'])
+    st.write('---')
+
+    if (data_file_1 is not None):
+        bank_raw = load_data(data_file_1)
+        bank = bank_raw.copy()
+
+        st.write('## Antes dos filtros')
+        st.write(bank_raw.head())
+
+        with st.sidebar.form(key='my_form'):
+
+            # SELECIONA O TIPO DE GRÁFICO
+            graph_type = st.radio('Tipo de gráfico:', ('Barras', 'Pizza'))
+
+            # IDADES
+            max_age = int(bank.age.max())
+            min_age = int(bank.age.min())
+            idades = st.slider(label='Idade',
+                               min_value=min_age,
+                               max_value=max_age,
+                               value=(min_age, max_age),
+                               step=1)
+
+            # PROFISSÕES
+            jobs_list = bank.job.unique().tolist()
+            jobs_list.append('All')
+            jobs_selected = st.multiselect("Profissão", jobs_list, ['All'])
+
+            # ESTADO CIVIL
+            marital_list = bank.marital.unique().tolist()
+            marital_list.append('All')
+            marital_selected = st.multiselect("Estado civil", marital_list, ['All'])
+
+            # DEFAULT?
+            default_list = bank.default.unique().tolist()
+            default_list.append('All')
+            default_selected = st.multiselect("Default", default_list, ['All'])
+
+            # TEM FINANCIAMENTO IMOBILIÁRIO?
+            housing_list = bank.housing.unique().tolist()
+            housing_list.append('All')
+            housing_selected = st.multiselect("Tem financiamento imob?", housing_list, ['All'])
+
+            # TEM EMPRÉSTIMO?
+            loan_list = bank.loan.unique().tolist()
+            loan_list.append('All')
+            loan_selected = st.multiselect("Tem empréstimo?", loan_list, ['All'])
+
+            # MEIO DE CONTATO?
+            contact_list = bank.contact.unique().tolist()
+            contact_list.append('All')
+            contact_selected = st.multiselect("Meio de contato", contact_list, ['All'])
+
+            # MÊS DO CONTATO
+            month_list = bank.month.unique().tolist()
+            month_list.append('All')
+            month_selected = st.multiselect("Mês do contato", month_list, ['All'])
+
+            # DIA DA SEMANA
+            day_of_week_list = bank.day_of_week.unique().tolist()
+            day_of_week_list.append('All')
+            day_of_week_selected = st.multiselect("Dia da semana", day_of_week_list, ['All'])
+
+            # encadeamento de métodos para filtrar a seleção
+            bank = (bank.query("age >= @idades[0] and age <= @idades[1]")
+                    .pipe(multiselect_filter, 'job', jobs_selected)
+                    .pipe(multiselect_filter, 'marital', marital_selected)
+                    .pipe(multiselect_filter, 'default', default_selected)
+                    .pipe(multiselect_filter, 'housing', housing_selected)
+                    .pipe(multiselect_filter, 'loan', loan_selected)
+                    .pipe(multiselect_filter, 'contact', contact_selected)
+                    .pipe(multiselect_filter, 'month', month_selected)
+                    .pipe(multiselect_filter, 'day_of_week', day_of_week_selected)
+                    )
+
+            submit_button = st.form_submit_button(label='Aplicar')
+
+        # Botões de download dos dados filtrados
+        st.write('## Após os filtros')
+        st.write(bank.head())
+
+        df_xlsx = to_excel(bank)
+        st.download_button(label='📥 Download tabela filtrada em EXCEL',
+                           data=df_xlsx,
+                           file_name='bank_filtered.xlsx')
+        st.markdown("---")
+
+        # PLOTS
+        fig, ax = plt.subplots(1, 2, figsize=(5, 3))
+
+        bank_raw_target_perc = bank_raw.y.value_counts(normalize=True).to_frame() * 100
+        bank_raw_target_perc = bank_raw_target_perc.sort_index()
+
+        try:
+            bank_target_perc = bank.y.value_counts(normalize=True).to_frame() * 100
+            bank_target_perc = bank_target_perc.sort_index()
+        except:
+            st.error('Erro no filtro')
+
+        # Botões de download dos dados dos gráficos
+        col1, col2 = st.columns(2)
+
+        df_xlsx = to_excel(bank_raw_target_perc)
+        col1.write('### Proporção original')
+        col1.write(bank_raw_target_perc)
+        col1.download_button(label='📥 Download',
+                             data=df_xlsx,
+                             file_name='bank_raw_y.xlsx')
+
+        df_xlsx = to_excel(bank_target_perc)
+        col2.write('### Proporção com filtros')
+        col2.write(bank_target_perc)
+        col2.download_button(label='📥 Download',
+                             data=df_xlsx,
+                             file_name='bank_y.xlsx')
+        st.markdown("---")
+
+        st.write('### Proporção de aceite em Gráficos')
+        # PLOTS
+        if graph_type == 'Barras':
+            sns.barplot(x=bank_raw_target_perc.index,
+                        y='y',
+                        data=bank_raw_target_perc,
+                        ax=ax[0])
+            ax[0].bar_label(ax[0].containers[0])
+            ax[0].set_title('Dados brutos',
+                            fontweight="bold")
+            ax[0].spines[['right', 'top']].set_visible(False)
+
+            sns.barplot(x=bank_target_perc.index,
+                        y='y',
+                        data=bank_target_perc,
+                        ax=ax[1])
+            ax[1].bar_label(ax[1].containers[0])
+            ax[1].set_title('Dados filtrados',
+                            fontweight="bold")
+            ax[1].spines[['right', 'top']].set_visible(False)
+        else:
+            bank_raw_target_perc.plot(kind='pie', autopct='%.2f', y='y', ax=ax[0])
+            ax[0].set_title('Dados brutos',
+                            fontweight="bold")
+
+            bank_target_perc.plot(kind='pie', autopct='%.2f', y='y', ax=ax[1])
+            ax[1].set_title('Dados filtrados',
+                            fontweight="bold")
+
+        st.pyplot(plt)
+
+
+if __name__ == '__main__':
+	main()
